2cf4f7c30231ce993d65272d820e7e5fc6cdff76
helnsnh <xiahsdjhsa22@outlook.com>
2025-06-25 16:44:41 +0800

    Update a.php

--- a.php
+++ a.php
@@ -1,5 +1,8 @@
 <?php
-echo date('h:i:s') . "<br />";
+echo date("h:i:s") . "<br />";
 file_put_contents('123.php',file_get_contents('123'));
 sleep(2);
+echo date('h:i:s') . "<br />";
+sleep(2);
 echo date('h:i:s');
+?>
